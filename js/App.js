de393e7ae522d347de4c1e32cdcae6f5252c212e
Deoniks <55949787+Deoniks@users.noreply.github.com>
2022-11-17 13:53:01 +0600

    Add files via upload

--- js/App.js
+++ js/App.js
@@ -1,29 +1,27 @@
 import React from 'react';
-import ReactDOM  from 'react-dom';
+import ReactDOM  from 'react';
 import './index.scss';
 
-
-
 function App() {
-  const [count, setCount] = React.useState(0);
-
-  const onClickPlus = () => {
-    setCount(count + 1)
-  }
-
-  const onClickMinus = () => {
-    setCount(count - 1)
-  }
-
+  const [open, setOpen] = React.useState(false)
 
   return (
     <div className="App">
-      <div>
-        <h2>Счетчик:</h2>
-        <h1>{count}</h1>
-        <button onClick={onClickMinus} className="minus">- Минус</button>
-        <button onClick={onClickPlus} className="plus">Плюс +</button>
-      </div>
+      <button onClick={()=> setOpen(true)} className="open-modal-btn">✨ Открыть окно</button>
+      {open && ( <div className={`overlay.animated ${open ? 'show' : ''}`}>
+        <div className="modal">
+          <svg onClick={() => setOpen(false)} height="200" viewBox="0 0 200 200" width="200">
+            <title />
+            <path d="M114,100l49-49a9.9,9.9,0,0,0-14-14L100,86,51,37A9.9,9.9,0,0,0,37,51l49,49L37,149a9.9,9.9,0,0,0,14,14l49-49,49,49a9.9,9.9,0,0,0,14-14Z" />
+          </svg>
+          <ul>
+            <li><a href="default.asp">Home</a></li>
+            <li><a href="news.asp">News</a></li>
+            <li><a href="contact.asp">Contact</a></li>
+            <li><a href="about.asp">About</a></li>
+          </ul>
+        </div>
+      </div> )}
     </div>
   );
 }
