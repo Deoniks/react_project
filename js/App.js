c67f04ba622e275474d7579f56c794d6bb60ab5a
Deoniks <55949787+Deoniks@users.noreply.github.com>
2022-11-22 18:04:13 +0600

    app files upd
    UPD files, change blocks...

--- js/App.js
+++ js/App.js
@@ -1,5 +1,4 @@
 import React from 'react';
-import ReactDOM  from 'react';
 import './index.scss';
 
 function App() {
@@ -12,14 +11,12 @@ function App() {
         <div className="modal">
           <svg onClick={() => setOpen(false)} height="200" viewBox="0 0 200 200" width="200">
             <title />
-            <path d="M114,100l49-49a9.9,9.9,0,0,0-14-14L100,86,51,37A9.9,9.9,0,0,0,37,51l49,49L37,149a9.9,9.9,0,0,0,14,14l49-49,49,49a9.9,9.9,0,0,0,14-14Z" />
           </svg>
-          <ul>
-            <li><a href="default.asp">Home</a></li>
-            <li><a href="news.asp">News</a></li>
-            <li><a href="contact.asp">Contact</a></li>
-            <li><a href="about.asp">About</a></li>
-          </ul>
+          <div className = "flexer">
+            <div>Main</div>
+            <div>News</div>
+            <div><button className='btn-flexe'>Login</button></div>
+          </div>
         </div>
       </div> )}
     </div>
